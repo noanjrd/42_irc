f8fbc4b543493c50e6d6925a7c588feda4b14ea0
Naziha <aankour.naziha@gmail.com>
2026-02-19 13:21:41 +0100

    suite bot bonus

--- srcs/bot/bot_ping.cpp
+++ srcs/bot/bot_ping.cpp
@@ -6,7 +6,7 @@
 /*   By: naankour <naankour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 14:08:58 by naankour          #+#    #+#             */
-/*   Updated: 2026/02/18 16:30:09 by naankour         ###   ########.fr       */
+/*   Updated: 2026/02/19 12:13:02 by naankour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,14 +47,31 @@ int main(int argc, char **argv)
 		return (1);
 	}	
 	
-	std::string pass = "PASS" + std::string(argv[3]) + "\r\n";
-	std::string nick = "NICK pingbot\r\\n";
+	std::string pass = "PASS " + std::string(argv[3]) + "\r\n";
+	std::string nick = "NICK pingbot\r\n";
 	std::string user = "USER pingbot 0 * :pingbot\r\n";
 
 	send(botFd, pass.c_str(), pass.size(), 0);
 	send(botFd, nick.c_str(), nick.size(), 0);
 	send(botFd, user.c_str(), user.size(), 0);
 
+	char buffer[512];
+	while(true)
+	{
+		std::memset(buffer, 0, sizeof(buffer));
+		int bytes = recv(botFd, buffer, sizeof(buffer) - 1, 0);
+		if (bytes <= 0)
+			break;
+		
+		std::string message = buffer;
+		std::cout << message << std::endl;
+		
+		if (message.find("PING") != std::string::npos)
+		{
+			std::string pong = "PONG" + message.substr(message.find("PING") + 4);
+			send(botFd, pong.c_str(), pong.size(), 0);
+		}
+	}
 	close(botFd);
-	return (01);	
+	return (0);	
 }
